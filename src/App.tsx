c0022a4628fd4c36ab7d8fba780275d7e6869e90
b_g <b_g@dsm.hs.kr>
2024-10-04 21:36:06 +0900

    feat: add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import Button from "./components/Button";
 
 const App = (): JSX.Element => {
-  return <>
-    <Button onClick={()=>{alert("버튼 클릭")}}>로그인</Button>
-  </>;
+  return <></>;
 };
 
 export default App;
