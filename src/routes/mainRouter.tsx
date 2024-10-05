11c23a14b229066d2c7e99d239cdb76ce37013e4
정승우 <ssounsowns@gmail.com>
2024-10-05 19:28:04 +0900

    feat: create MainRouter

---
+++ src/routes/mainRouter.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Login from "../pages/login";
+import SignUp from "../pages/signUp";
+import PwChange from "../pages/pwChange";
+import PwFind from "../pages/pwFind";
+
+const MainRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/pwFind" element={<PwFind />} />
+        <Route path="/pwChange" element={<PwChange />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default MainRouter;
