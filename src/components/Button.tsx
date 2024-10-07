55c077a6d3408c3649244207adb194f7ab6a5d70
b_g <b_g@dsm.hs.kr>
2024-10-07 22:43:49 +0900

    refactor: refactor button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+interface ButtonProps {
+  children: string;
+  disabled: boolean;
+  onClick: () => void;
+}
+
+const Button = ({
+  children,
+  disabled = false,
+  onClick,
+}:ButtonProps) => {
+  return (
+    <ButtonBase disabled={disabled} onClick={onClick}>
+      {children}
+    </ButtonBase>
+  );
+};
+
 const ButtonBase = styled.button`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  vertical-align: center;
-  position: relative;
   min-width: 64px;
   max-width: 450px;
   width: 100%;
@@ -28,31 +44,11 @@ const ButtonBase = styled.button`
   font-weight: 700;
 
   line-height: 1.2;
-  white-space: nowrap;
   overflow: hidden;
-  text-overflow: ellipsis;
 
   user-select: none;
 
   transition: background-color 0.1s ease;
 `;
-
-interface ButtonProps {
-  children: string;
-  disabled?: boolean;
-  onClick: () => void;
-}
-
-const Button: React.FC<ButtonProps> = ({
-  children,
-  disabled = false,
-  onClick,
-}) => {
-  return (
-    <ButtonBase disabled={disabled} onClick={onClick}>
-      {children}
-    </ButtonBase>
-  );
-};
 
 export default Button;
