8cc54e07c5b33839e4a44806b34892883ab27feb
정승우 <ssounsowns@gmail.com>
2024-10-14 22:40:39 +0900

    feat: 회원가입 퍼블리싱 완성

--- src/pages/signUp.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-const SignUp = (): JSX.Element => {
-  return <></>;
-};
-
-export default SignUp;
---
+++ src/pages/signUpPage.tsx
@@ -0,0 +1,19 @@
+import SignUp from "../components/signUp";
+import styled from "styled-components";
+
+const SignUpPage = (): JSX.Element => {
+  return (
+    <Wrapper>
+      <SignUp />
+    </Wrapper>
+  );
+};
+
+export default SignUpPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100dvh;
+  align-items: center;
+`;
