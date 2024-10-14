58c3536debf6c3832a9127e847aa9e96430ca02b
정승우 <ssounsowns@gmail.com>
2024-10-14 22:41:36 +0900

    refactor: 로그인 페이지 컴포넌트 분리

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -25,47 +25,41 @@ const Login = (): JSX.Element => {
   const onClick = () => {};
   return (
     <Wrapper>
-      <Container>
-        <Field>
-          <LogoContainer>
-            <LogoBox>
-              <Logo src={LogoImg} />
-            </LogoBox>
-            <InputContainer>
-              <TextInput
-                label="이메일"
-                onChange={onEmail}
-                value={email}
-                errorMessage="유효하지 않은 이메일 입니다"
-                placeholder="@dsm.hs.kr"
-              />
+      <LogoContainer>
+        <LogoBox>
+          <Logo src={LogoImg} />
+        </LogoBox>
+        <InputContainer>
+          <TextInput
+            label="이메일"
+            onChange={onEmail}
+            value={email}
+            errorMessage="유효하지 않은 이메일 입니다"
+            placeholder="@dsm.hs.kr"
+          />
 
-              <PasswordInput
-                label="비밀번호"
-                onChange={onPassword}
-                value={password}
-                errorMessage="비밀번호가 올바르지 않습니다."
-              />
-            </InputContainer>
-          </LogoContainer>
-          <Footer>
-            <LoginOption>
-              <BtnBox>
-                <Button children="로그인" onClick={onClick} />
-              </BtnBox>
-              <PsFind onClick={() => navigate("/pwFind")}>
-                비밀번호 찾기 <img src={Arrow} />
-              </PsFind>
-            </LoginOption>
-            <NoAccount>
-              계정이 없다면?
-              <GoSignUp onClick={() => navigate("/signUp")}>
-                회원가입 하기
-              </GoSignUp>
-            </NoAccount>
-          </Footer>
-        </Field>
-      </Container>
+          <PasswordInput
+            label="비밀번호"
+            onChange={onPassword}
+            value={password}
+            errorMessage="비밀번호가 올바르지 않습니다."
+          />
+        </InputContainer>
+      </LogoContainer>
+      <Footer>
+        <LoginOption>
+          <BtnBox>
+            <Button children="로그인" onClick={onClick} />
+          </BtnBox>
+          <PsFind onClick={() => navigate("/pwFind")}>
+            비밀번호 찾기 <img src={Arrow} />
+          </PsFind>
+        </LoginOption>
+        <NoAccount>
+          계정이 없다면?
+          <GoSignUp onClick={() => navigate("/signUp")}>회원가입 하기</GoSignUp>
+        </NoAccount>
+      </Footer>
     </Wrapper>
   );
 };
@@ -94,12 +88,6 @@ const PsFind = styled.div`
   justify-content: end;
   gap: 7px;
   cursor: pointer;
-`;
-
-const Field = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 300px;
 `;
 
 const Footer = styled.div`
@@ -138,19 +126,13 @@ const BtnBox = styled.div`
   height: 81px;
 `;
 
-const Container = styled.div`
-  background-color: #ffffff;
+const Wrapper = styled.div`
   border-radius: 50px;
   padding: 62px 158px;
+  background-color: #ffffff;
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100dvh;
-  align-items: center;
+  gap: 183px;
 `;
