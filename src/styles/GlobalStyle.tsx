7125e53655548796a4c74034f7c9593677358d5a
정승우 <ssounsowns@gmail.com>
2024-10-16 22:50:57 +0900

    chore: font 설정 위치 수정
    * -> body

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -4,11 +4,11 @@ import reset from "styled-reset";
 const GlobalStyles = createGlobalStyle`
   ${reset};
   *{
-    font-family: 'Inter', sans-serif;
     box-sizing:border-box;
   }
   body{
     background-color:#F9FAFB;
+    font-family: 'Inter', sans-serif;
 
   }
 `;
