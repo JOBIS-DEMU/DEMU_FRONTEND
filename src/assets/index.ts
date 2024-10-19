5a705a0c895b9e8ae97bc58a8ccece0bf0811e68
정승우 <ssounsowns@gmail.com>
2024-10-19 22:02:48 +0900

    feat: add Icon
    헤더 메뉴 개발에 필요한 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,6 @@ export { default as Invisible } from "./invisible.svg";
 export { default as LogoImg } from "./logo.svg";
 export { default as TitleLogo } from "./titlelogo.svg";
 export { default as Visible } from "./visible.svg";
+export { default as HeaderIcon } from "./headerMenuIcon.svg";
+export { default as SearchIcon } from "./searchIcon.svg";
+export { default as LineOption } from "./lineOption.svg";
