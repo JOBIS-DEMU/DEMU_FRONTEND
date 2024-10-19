91addbd60975abbbe0920200f6418d5620217139
정승우 <ssounsowns@gmail.com>
2024-10-19 22:03:59 +0900

    feat: add headerMenu
    헤더 메뉴 컴포넌트 개발

---
+++ src/components/headerMenu.tsx
@@ -0,0 +1,88 @@
+import { useNavigate } from "react-router-dom";
+import { HeaderIcon, LineOption, SearchIcon } from "../assets/index";
+import styled from "styled-components";
+
+const HeaderMenu = () => {
+  const navigate = useNavigate();
+  const onHome = () => {
+    navigate("/home");
+  };
+  const onMyPage = () => {
+    navigate("/myPage");
+  };
+  const onSwap = () => {
+    navigate("/");
+  };
+  return (
+    <Wrapper>
+      <Container>
+        <img src={HeaderIcon} />
+        <InputBox>
+          <SearchInput placeholder="검색" />
+          <SearchImg src={SearchIcon} />
+        </InputBox>
+      </Container>
+      <Option>
+        <Selection onClick={onHome}>홈</Selection>
+        <img src={LineOption} />
+        <Selection onClick={onMyPage}>마이페이지</Selection>
+        <img src={LineOption} />
+        <Selection onClick={onSwap}>로그인</Selection>
+      </Option>
+    </Wrapper>
+  );
+};
+
+export default HeaderMenu;
+
+const Selection = styled.span`
+  cursor: pointer;
+`;
+
+const Option = styled.div`
+  display: flex;
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 24px;
+  gap: 15px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  gap: 30px;
+  height: 76px;
+`;
+
+const SearchImg = styled.img`
+  margin: 17px;
+  cursor: pointer;
+`;
+
+const Wrapper = styled.div`
+  background-color: #1b69ff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 44px 102px 44px 142px;
+  width: 100%;
+`;
+const InputBox = styled.div`
+  background-color: #1b69ff;
+  display: flex;
+  align-items: center;
+  border: 1.8px solid #707583;
+`;
+
+const SearchInput = styled.input`
+  padding: 24px 0px 24px 36px;
+  outline: none;
+  border: none;
+  width: 716px;
+  height: 76px;
+
+  font-size: 24px;
+  &::placeholder {
+    font-size: 24px;
+    font-family: "Inter", sans-serif;
+  }
+`;
