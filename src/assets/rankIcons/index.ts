16371e35a6533ef077157cac7ad6efca7fe4436c
seungWoo <ssounsowns@gmail.com>
2024-10-26 00:44:17 +0900

    feat: add rankIcons
    랭크 아이콘 추가

---
+++ src/assets/rankIcons/index.ts
@@ -0,0 +1,5 @@
+export { default as Bronze } from "./bronze.svg";
+export { default as Silver } from "./sliver.svg";
+export { default as Gold } from "./gold.svg";
+export { default as Platinum } from "./platinum.svg";
+export { default as Diamond } from "./diamond.svg";
