f3da859f725d97be64d988f7310507c0f8d5095e
seungWoo <ssounsowns@gmail.com>
2024-10-25 21:21:43 +0900

    feat: create filterTab
    전공 블로그를 전공 별로 볼 수 있는 필터탭을 퍼블리싱 함

--- src/components/homePage/mainTab.tsx
+++ src/components/homePage/mainTab.tsx
@@ -2,101 +2,101 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const MainTab = () => {
-  const [select, setSelect] = useState(false);
+  const [selectIndex, setSelectIndex] = useState<number | null>(null);
+
+  const Option = [
+    {
+      name: "backend",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "frontend",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "iOS",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "design",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "AI",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "flutter",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "AOS",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "embedded",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "full stack",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "game",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "devops",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "security",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "전체 글",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+    {
+      name: "기타 전공",
+      clickColor: "#1B69FF",
+      color: "#707583",
+    },
+  ];
+
+  const onSelectIndex = (index: number) => {
+    setSelectIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
 
   return (
     <Wrapper>
       <FilterBox>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          backend
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          fronted
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          iOS
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          design
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          AI
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          flutter
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          AOS
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          embedded
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          full stack
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          game
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          devops
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          security
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          전체 글
-        </Select>
-        <Select
-          onClick={() => setSelect(!select)}
-          color={select ? "#1B69FF" : "#707583"}
-        >
-          기타 전공
-        </Select>
+        {Option.map(({ name, clickColor, color }, index) => (
+          <Select
+            key={index}
+            color={selectIndex === index ? clickColor : color}
+            onClick={() => onSelectIndex(index)}
+          >
+            {name}
+          </Select>
+        ))}
       </FilterBox>
     </Wrapper>
   );
 };
-
-export default MainTab;
 
 const Wrapper = styled.div`
   margin-left: 142px;
@@ -106,13 +106,15 @@ const FilterBox = styled.div`
   gap: 19px;
   border-top: 1px solid #707583;
   border-bottom: 1px solid #707583;
-  padding: 2px 0px;
+  padding: 2px 0px 0px 0px;
   width: fit-content;
-  height: 31px;
+  height: fit-content;
 `;
 const Select = styled.span`
   cursor: pointer;
   font-size: 24px;
   font-weight: 600;
-  color: ${proops => proops.color};
+  color: ${(proops) => proops.color};
 `;
+
+export default MainTab;
