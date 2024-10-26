915db8c04ac872b42cd0c765cc496724236241fb
seungWoo <ssounsowns@gmail.com>
2024-10-26 19:18:55 +0900

    feat: create home page
    퍼블리싱 완료

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,23 +11,38 @@ import styled from "styled-components";
 const Home = () => {
   return (
     <Wrapper>
-      <HeaderMenu />
-      <PopularTab />
-      <FilterTab />
-      <Main>
-        <Post />
-        <LoginTab />
-      </Main>
-      <PageSwitch />
+      <Field>
+        <HeaderMenu />
+        <PopularTab />
+        <FilterTab />
+        <Main>
+          <Post />
+          <LoginTab />
+        </Main>
+        <PageSwitch />
+      </Field>
+      <Footer></Footer>
     </Wrapper>
   );
 };
 
 export default Home;
-const Wrapper = styled.div`
+
+const Footer = styled.div`
+  width: 100%;
+  height: 160px;
+  background-color: #e9e9e9;
+`;
+
+const Field = styled.div`
   display: flex;
   flex-direction: column;
   gap: 37px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const Main = styled.div`
