1a290471b383a0fcaf99d6723e30f5cc00d155a3
seungWoo <ssounsowns@gmail.com>
2024-10-29 00:17:34 +0900

    feat: create myPost
    마이페이지에서 사용자가 작성한 글을 보여주는 컴포넌트 제작

--- src/components/myPage/index.ts
+++ src/components/myPage/index.ts
@@ -1,2 +1,4 @@
 export { default as ProgressBar } from "./progressBar";
 export { default as HeaderMenu } from "./headerMenu";
+export { default as MyPost } from "./myPost";
+export { default as myPageLoginTab } from "./myPageLoginTab";
