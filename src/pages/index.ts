602d9857a4c05db058a29e98392e3c50a232191e
seungWoo <ssounsowns@gmail.com>
2024-10-30 17:20:08 +0900

    rename: PwFind와 SignUp의 이름을 수정
    뒤에 Page를 붙임

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
 export { default as HomePage } from "./homePage";
 export { default as LoginPage } from "./loginPage";
 export { default as MyPage } from "./myPage";
-export { default as PwFind } from "./pwFind";
-export { default as SignUp } from "./signUpPage";
+export { default as PwFindPage } from "./pwFindPage";
+export { default as SignUpPage } from "./signUpPage";
 export { default as WritePage } from "./writePage";
 export { default as MyBlogPage } from "./myBlogPage";
