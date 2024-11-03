08e48bab40e90ebb3c768db028741217d27f1221
b_g <b_g@dsm.hs.kr>
2024-11-03 21:35:59 +0900

    fix: fix usermodel from api change

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,19 +2,17 @@ import Grade from "src/types/grade";
 import Major from "src/types/major";
 
 class UserModel {
-  email: String;
   name: String;
   description: String;
   point: number;
-  tier: Grade;
+  grade: Grade;
   major: Major;
 
-  constructor(email: String, name: String, description?: String, point?: number, tier?: Grade, major?: Major) {
-    this.email = email;
+  constructor(name: String, description?: String, point?: number, tier?: Grade, major?: Major) {
     this.name = name;
     this.description = description ?? "";
     this.point = point ?? 0;
-    this.tier = tier ?? Grade.BRONZE;
+    this.grade = tier ?? Grade.BRONZE;
     this.major = major ?? Major.NONE;
   }
 }
