3e6c637513e0f1e677a9be2048d76ddce3a2e20d
b_g <b_g@dsm.hs.kr>
2024-11-04 19:19:37 +0900

    feat: add util functions for grade type

--- src/types/grade.ts
+++ src/types/grade.ts
@@ -1,3 +1,5 @@
+import { Bronze, Silver, Gold, Platinum, Diamond } from "../assets/rankIcons";
+
 enum Grade {
   BRONZE = "bronze",
   SILVER = "silver",
@@ -5,5 +7,24 @@ enum Grade {
   PLATINUM = "platinum",
   DIAMOND = "diamond"
 };
+
+function getRankIcon(gradeStr: string) {
+  const grade = gradeStr.toLowerCase() as Grade;
+  
+  switch (grade) {
+    case Grade.BRONZE:
+      return Bronze;
+    case Grade.SILVER:
+      return Silver;
+    case Grade.GOLD:
+      return Gold;
+    case Grade.PLATINUM:
+      return Platinum;
+    case Grade.DIAMOND:
+      return Diamond;
+  }
+}
+
+export { getRankIcon };
 
 export default Grade;
